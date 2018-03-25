21704ae5b0e9122505795cbb00731e6c737dc75d
NightRoadIx <quintanarxbio@gmail.com>
2018-03-25 00:41:21 -0600

    Add files via upload
    Código en ensamblador, ejemplo 00

---
+++ ejem00.asm
@@ -0,0 +1,28 @@
+.586								; Procesador objetivo, usar instrucciones para máquinas de clase Pentium (x86 32 bits)
+.model flat, stdcall				; Utilizar el modelo de memoria flat (plano), llamadas de convención stdcall, para usar win32
+.stack 4096 						; Definir el stack (pila) de 4096 bytes (se reserva un espacio en memoria de ese tamaño)
+
+extrn ExitProcess@4:proc			; Funciones de la API Win32 (@bytes que toman sus parámetros)
+extrn MessageBoxA@16:proc			; Son salida del proceso y una ventana de mensaje
+
+.data								; Crear un segmento de datos cercano, las variables locales se especifican después
+msg_txt db "Hola a Todos",0			; Aquí se define como byte el mensaje de texto (string) y un 0
+msg_caption db "Programa Prueba",0	; Aquí se define como byte el título del mensaje (string) y un 0
+
+.code								; Indica el comienzo del segmento de código
+	main:							; Etiqueta main
+		nop							; No Operation
+		
+		push 0						; Enviar un 0 a la pila		
+		lea eax, msg_caption		; Cargar dirección efectiva destino, fuente, el mensaje al registro de propósito general EAX
+		push eax					; Mover el contenido del registro EAX a la pila		
+		lea eax, msg_txt			; Cargar el registro EAX con el mensaje
+		push eax					; Mover a la pila		
+		push 0						; Enviar un cero a la pila
+									; en este punto la pila esta cargada con los valores (0, msg_caption, msg_txt, 0)
+									; esto indica que el proceso MessageBoxA recibe como parámetros (0, msg_txt, msg_caption, 0)
+		call MessageBoxA@16			; Llamada a la función de la API
+		
+		push 0						; La siguiente función requiere de un 0
+		call ExitProcess@4			; Llamada a la función de la API ( exit(0) )
+	end main						; fin de esta sección de código y del programa
