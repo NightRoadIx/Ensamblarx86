91bb3e8b85c65fb171be5c0b5df86fa16c3ba9a8
NightRoadIx <quintanarxbio@gmail.com>
2018-03-29 16:42:57 -0600

    Update ejem00.asm

--- ejem00.asm
+++ ejem00.asm
@@ -1,28 +1,33 @@
-.586								; Procesador objetivo, usar instrucciones para máquinas de clase Pentium (x86 32 bits)
+.586						; Procesador objetivo, usar instrucciones para máquinas de clase Pentium (x86 32 bits)
 .model flat, stdcall				; Utilizar el modelo de memoria flat (plano), llamadas de convención stdcall, para usar win32
-.stack 4096 						; Definir el stack (pila) de 4096 bytes (se reserva un espacio en memoria de ese tamaño)
+.stack 4096 					; Definir el stack (pila) de 4096 bytes (se reserva un espacio en memoria de ese tamaño)
 
 extrn ExitProcess@4:proc			; Funciones de la API Win32 (@bytes que toman sus parámetros)
 extrn MessageBoxA@16:proc			; Son salida del proceso y una ventana de mensaje
 
-.data								; Crear un segmento de datos cercano, las variables locales se especifican después
+.data						; Crear un segmento de datos cercano, las variables locales se especifican después
 msg_txt db "Hola a Todos",0			; Aquí se define como byte el mensaje de texto (string) y un 0
-msg_caption db "Programa Prueba",0	; Aquí se define como byte el título del mensaje (string) y un 0
+msg_caption db "Programa Prueba",0		; Aquí se define como byte el título del mensaje (string) y un 0
 
-.code								; Indica el comienzo del segmento de código
-	main:							; Etiqueta main
-		nop							; No Operation
+.code						; Indica el comienzo del segmento de código
+	main:					; Etiqueta main
+		nop				; No Operation
 		
-		push 0						; Enviar un 0 a la pila		
+		push 32				; Enviar a la pila que tipo de botones se utilizan  {uType}
+						; Esto se calcula como la suma de las constantes para botones, íconos, botón default y modalidad
+						; 	[BOTONES]   vbOKOnly (0), vbOKCancel(1), vbAbortRetryIgnore (2), vbYesNoCancel(3), vbYesNo (4), vbRetryCancel (5)
+						;	[ICONOS]    vbCritical (16),vbQuestion (32),vbExclamation (48), vbInformation (64)
+						;	[DEFAULT]   vbDefaultButton1 (0),vbDefaultButton2 (256),vbDefaultButton3 (512),vbDefaultButton4(768)
+						;	[MODALIDAD] vbApplicationModal, vbSystemModal
 		lea eax, msg_caption		; Cargar dirección efectiva destino, fuente, el mensaje al registro de propósito general EAX
-		push eax					; Mover el contenido del registro EAX a la pila		
-		lea eax, msg_txt			; Cargar el registro EAX con el mensaje
-		push eax					; Mover a la pila		
-		push 0						; Enviar un cero a la pila
-									; en este punto la pila esta cargada con los valores (0, msg_caption, msg_txt, 0)
-									; esto indica que el proceso MessageBoxA recibe como parámetros (0, msg_txt, msg_caption, 0)
-		call MessageBoxA@16			; Llamada a la función de la API
+		push eax			; Mover el contenido del registro EAX a la pila {lpCaption}
+		lea eax, msg_txt		; Cargar el registro EAX con el mensaje
+		push eax			; Mover a la pila {lpText}
+		push 0				; Enviar un cero a la pila, {hWnd}
+						; en este punto la pila esta cargada con los valores (hWnd, lpText, lpCaption, k)
+						; esto indica que el proceso MessageBoxA recibe como parámetros (hWnd, lpText, lpCaption, k)
+		call MessageBoxA@16		; Llamada a la función de la API
 		
-		push 0						; La siguiente función requiere de un 0
-		call ExitProcess@4			; Llamada a la función de la API ( exit(0) )
-	end main						; fin de esta sección de código y del programa
+		push 0				; La siguiente función requiere de un 0
+		call ExitProcess@4		; Llamada a la función de la API ( exit(0) )
+	end main				; fin de esta sección de código y del programa
