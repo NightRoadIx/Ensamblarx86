30ab9a418b1d679cca8087c650d118024516387b
NightRoadIx <quintanarxbio@gmail.com>
2018-04-03 18:51:02 -0500

    Update ejem01.asm

--- ejem01.asm
+++ ejem01.asm
@@ -1,24 +1,24 @@
-.586								; Procesador objetivo
+.586						; Procesador objetivo
 .model flat, stdcall				; Modelo de memoria plano, llamadas convencionales o estándar
 
-.stack 4096							; Sección de la pila
+.stack 4096					; Sección de la pila
 
 extrn ExitProcess@4:proc			; Funciones de la API Win32 (@bytes que toman sus parámetros)
 
-.data								; Sección de datos
+.data						; Sección de datos
 
-.code								; Sección del código
-	main:							; Etiqueta que define un segmento del código del programa
-		nop							; Sin operación
-		push 0						; Empujar un 0 a la pila
+.code						; Sección del código
+	main:					; Etiqueta que define un segmento del código del programa
+		nop				; Sin operación
+		push 0				; Empujar un 0 a la pila
 
-		mov eax, 72h				; Ingresar un número directo al registro (decimal, hexadecimal, binario)
-		mov ebx, eax				; Mover datos entre registros
-		add eax, ebx				; Sumar los valores en los registros
+		mov eax, 72h			; Ingresar un número directo al registro (decimal, hexadecimal, binario)
+		mov ebx, eax			; Mover datos entre registros
+		add eax, ebx			; Sumar los valores en los registros
 
-		mov eax, [40100H]			; Mover los datos en la dirección 0x40100 al registro eax
+		mov eax, [40100H]		; Mover los datos en la dirección 0x40100 al registro eax
 
-		push 0						; Mandar un 0 a la pila (argumento utilizado por el proceso siguiente)
-		call ExitProcess@4			; Llamada a la función de la API
+		push 0				; Mandar un 0 a la pila (argumento utilizado por el proceso siguiente)
+		call ExitProcess@4		; Llamada a la función de la API
 
-	end main						; Terminar la etiqueta
+	end main				; Terminar la etiqueta
