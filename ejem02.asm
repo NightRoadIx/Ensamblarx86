6aeb8ca401d6c809ef84cada9b43610ed777b885
NightRoadIx <quintanarxbio@gmail.com>
2018-07-01 21:52:52 -0600

    Update ejem02.asm

--- ejem02.asm
+++ ejem02.asm
@@ -1,46 +1,131 @@
-.586						; Procesador objetivo, usar instrucciones para máquinas de clase Pentium
-; .386 .486  -- 16 bits
-.model flat, stdcall				; Utilizar el modelo de memoria flat (plano), llamadas de convención stdcall, para usar win32
-; directiva plana, llamadas estándar (win32)
-.stack 4096 					; Definir el stack (pila) de 4096 bytes
+.586
+.model flat, stdcall
+.stack 4096
 extrn ExitProcess@4:proc			; Funciones de la API Win32 (@bytes que toman sus parámetros)
 
-.data						; Crear un segmento de datos cercano, las variables locales se especifican después
-	var		DB	64		; Declarar un byte, referido como una locación var, conteniendo el valor 64
-	; byte var = 64;   {{8 bits}}
-	var2	DB	?			; Declarar un byte sin iniciar, referido como locación var2
-	; byte var2;
-			DB	10		; Declarar un byte sin etiqueta, que contiene el valor 10. Su locación es var2+1
-	; byte = 10;
-	X		DW	?		; Declarar un valor de 2-bytes (palabra) sin iniciar, referida como X
-	; word X;   {{16 bits}}
-	Y		DD	30000		; Declarar un valor de 4-bytes, referido como locación Y, iniciado a 30000
-	; double_word Y = 30000;	{{32 bits}}
-	Z		DD	1, 2, 3		; Declarar 3 valores de 4-bytes, iniciados en 1, 2 y 3. El valor de la locación Z+8 es 3
-	; double_word Z[] = {1, 2, 3};
-	arr		DD	100 DUP(0)	; Declarar 100 valores de 4-bytes iniciando en la locación arr, todos iniciados a 0
-	; double_word arr[100] = {0,0,0,...,0};
+.data
+	res	DD	?			;
 	
-.code						; Indica el comienzo del segmento de código
-	main:					; Etiqueta main
-		nop				; No Operation
-		push 0				; RAMEND (último elemento de memoria)
-		
-		mov EAX, 0FFh			; Mover un número a un registro
-						; move EAX, 255		
-		mov [var2], 020h		; Mover el valor 020H a la variable var2
-		mov EBX, EAX			; Mover el valor de registro EAX al registro EBX
-
-		mov byte ptr [var], 5		; Almacenar el valor de 5 en un byte en la locación var
-						; Al utilizar [] quiere decir que no se quiere la locación del identificador, sino su valor
-		
-		push eax			; Enviar eax a la pila
+.code
+	main:
 		nop
-		pop edi				; Obtener el valor que se encuentra en el pila y vaciarlo al registro EDI
-		
-		lea ebx, [var]			; Cargar la dirección Efectiva de la variable (probar ebx, var)
-		lea ecx, [eax]			; Esto funciona como mov (probar ecx, eax)
-		
 		push 0
-		call ExitProcess@4		; Llamada a la función de la API
-	end main				; fin de esta sección de código y del programa
+		
+		; REGISTROS DE PROPÓSITO GENERAL EAX, EBX, ECX, EDX
+		; Estos son registros de 32 bits, sin embargo se subdividen en registros de 8 y 16 bits
+		; para contar con una mayor cantidad de registros
+		; ES MUY IMPORTANTE OBSERVAR COMO SE MUEVEN LOS DATOS DE UN REGISTRO A OTRO
+		; ¡PUES ES IMPOSIBLE HACER CABER UN VALOR DE 32 BITS EN UN REGISTRO DE 8 BITS!
+		mov eax, 0			; Mover un 0 al registro EAX de 32 bits (limpiar todo el registro)
+		mov al, 8			; Mover un valor de 8 bits a la parte "baja" del registro AX
+		mov ah, 1			; Mover un valor de 8 bits a la parte "alta" del registro AX
+		; En este momento el registro debe valer 0x0108
+		mov ax, 1492		; Mover un valor de 16 bits al registro AX
+		mov eax, 14000605	; Mover un valor de 32 bits al registro EAX
+		
+		; entonces simplemente los 4 registros disponibles de propósito general (EAX, EBX, ECX, EDX)
+		; se subdividen en:
+		; 
+		;  EAX (32 bits)
+		; ---------------------------------------
+		; |                  |   AH    |   AL   |  (cada uno de 8 bits)
+		; ---------------------------------------
+		;		    AX (16 bits)
+		
+		; MANEJO BÁSICO DE LA ALU, OPERACIONES ARTIMÉTICO LÓGICAS (Y DESPLAZAMIENTO DE BITS)
+		; [[ PONER MUCHA ATENCIÓN A LOS BITS DEL REGISTRO DE ESTADO ]]
+		
+		; ******* OPERACIONES ARITMÉTICAS *******
+		mov eax, 1			; Cargar datos en registro eax (MOVer datos)
+		mov ebx, 5			; Cargar datos en registro ebx
+
+		inc eax				; INCrementar valor del registro eax en 1									
+		dec ebx				; DECrementar valor del registro ebx en 1
+						; INC operando, donde operando puede ser registro o valor en memoria
+						; DEC operando
+
+		add eax, ebx			; Suma de los datos que se encuentran en los registros eax, ebx									
+		sub eax, ebx			; SUBstraer al valor del registro eax, el valor del registro ebx
+						; ADD destino, fuente.	Pueden ser:
+						; Registro a Registro			ADD reg, reg
+						; Memoria a registro			ADD mem, reg
+						; Registro a memoria			ADD reg, mem
+						; Registro a dato constante		ADD reg, inmediato
+		mov ecx, eax			; Mover el resultado del registro eax a la variable en memoria res
+				
+		mul ebx				; MULtiplicar el valor en el registro eax por ebx
+		; La multiplicación se puede presentar en 3 casos
+		; 1.- Multiplicar dos bytes
+		; Se considera que uno de los multiplicandos está en el registro AL [Este es el registro principal de la ALU, el acumulador, es fijo]
+		; El otro en algún registro de 8 bits (BL, BH, CL, CH, DL, DH e incluso AH)
+		; Si se quiere multiplicar AL x BL se coloca:
+		; mul bl
+		; El resultado se guarda en AH:AL
+
+		; 2.- Multiplicar dos words
+		; Multiplicando en AX, mientras que el otro en cualquier registro de tamaño word (BX, CX, DX)
+		; La operación es AX x BX = DX:AX 
+		; mul bx
+
+		; 3.- La operación realizada en el ejemplo, 2 palabras dobles
+		; se hace: EAX x EBX = EDX:EAX
+
+		mov ebx, 3
+		div ebx			; DIVidir el valor en el registro eax entre ebx
+		; Para la división hay igual 3 casos:
+		; 1.- Dividir bytes
+		; El dividendo es un word almacenado en AX y se divide por algún byte (por ejemplo BL)
+		; AX / BL = AL		y el residuo queda en AH
+
+		; 2.- Dividir words
+		; El dividendo es una dword almacenada en los registros DX:AX y se divide por alguna word (por ejemplo BX)
+		; DX:AX / BX = AX	y el residuo en DX
+
+		; 3.- Dividir dwords (ejemplo)
+		; El dividendo es una qword almacenada en EDX:EAX y el divisor es una dword (por ejemplo EBX)
+		; EDX:EAX / EBX = EAX	y el residuo en EDX
+
+		; ******* OPERACIONES LÓGICAS *******
+
+		mov eax, 170		; Mover valor 170 al registro eax
+		and eax, 15		; Hacer eax = eax AND 15
+					; Recordar que la operación AND se efectua bit a bit sobre ambos operandos
+					; Esto quiere decir:
+					; 170 = 10101010b = 0xAA
+					;  15 = 00001111b = 0x0F
+					; -----------------------
+					;  10 = 00001010b = 0x0A  (A esto se le conoce como "enmascaramiento")
+
+		mov eax, 170		; Mover valor 170 al registro eax
+		or eax, 15		; Hacer eax = eax OR 15
+					; Recordar que la operación OR se efectua bit a bit sobre ambos operandos
+					; Esto quiere decir:
+					; 170 = 10101010b = 0xAA
+					;  15 = 00001111b = 0x0F
+					; -----------------------
+					; 175 = 10101111b = 0xAF
+
+		mov eax, 170		; Mover valor 170 al registro eax
+		xor eax, 15		; Hacer eax = eax XOR 15
+					; Recordar que la operación AND se efectua bit a bit sobre ambos operandos
+					; Esto quiere decir:
+					; 170 = 10101010b = 0xAA
+					;  15 = 00001111b = 0x0F
+					; -----------------------
+					; 165 = 10100101b = 0xA5	
+
+		mov eax, 170		; Mover valor 170 al registro eax
+		not eax			; Hacer eax = negado(eax)
+
+		; OPERACIONES DE DESPLAZAMIENTO DE BITS
+
+		mov eax, 48		; Mover valor 48 al regisro eax
+		shr eax, 3		; Desplazamiento de 3 bits a la derecha
+					; shl desplazamiento de bits a la izquierda
+
+		ror eax, 7		; Rotación de 7 bits a la derecha
+					; rol rotación de bits a la izquierda
+
+		push 0
+		call ExitProcess@4	; Llamada a la función de la API
+	end main
